67151c326779c13a14495b27c1b178c64b0e03ce
fardinbh <45644125+fardinbh@users.noreply.github.com>
2023-05-29 17:51:14 -0400

    Create Setup.py

---
+++ Setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dgpy",
+    version="1.0.0",
+    author="FardinBahreini",
+    author_email="fardinbhi@gmail.com",
+    description="A library for creating and calculating graphs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fardinbh/dgpy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
